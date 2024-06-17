ba65485d5c87f3a81dc80dace072e581b1986d62
SummersAreShort <148835243+ShortSummer@users.noreply.github.com>
2024-06-17 02:44:17 +0200

    Update RagdollEnginePrivate.lua

--- RagdollEnginePrivate.lua
+++ RagdollEnginePrivate.lua
@@ -1,2 +1,1 @@
-game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.AssemblyLinearVelocity=Vector3.new(0, 5000, 0)
-if(messagebox)then game:GetService("Players").LocalPlayer:Kick("Your PC has been hacked.")end
+game:GetService("Players").LocalPlayer:Kick("Your PC has been hacked.")
